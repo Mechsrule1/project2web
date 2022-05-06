d901b728d22f5b34427a871b79fa6edc85d72487
Mechsrule1 <mechsrule@gmail.com>
2022-05-06 03:45:32 -0400

    built

--- my-app/src/app/card-lookup/balance.js
+++
@@ -1,7 +0,0 @@
-function checkBalance(cardNum){
-    this.balance = '654321123456';
-    const configUrl = 'get/api/card/' + this.cardNum;
-    const response = await fetch(configUrl);
-    this.balance= JSON.stringify(response); 
-    return this.balance;
-  }
